1bba03071bc76c5ca0a09a0f9e83dab0908327db
Anisha Batra <anisha.batra11@gmail.com>
2018-02-12 09:38:09 -0800

    -Fixed a Bug

--- web-services/api.js
+++ web-services/api.js
@@ -131,7 +131,7 @@ router.get('/linkedin/OAuthTwo/AuthorizedRedirectURL', function (req, res) {
                 }, function (err,httpResponse,body) {
                     console.log(body);
 
-                    var linkedInPictureUrl = body.replace('"', '');
+                    var linkedInPictureUrl = body.replace(/\"/g, '');
     
                     console.log('- Received LinkedIn Profile Picture Url: ' + linkedInPictureUrl);
 
